9a6a273ac21b612ab6f03a926c8a5b7abcbcecf7
ali <121182316+aliw14@users.noreply.github.com>
2023-05-03 16:18:59 +0400

    Add files via upload

---
+++ uni bank/js/resources.js
@@ -0,0 +1,46 @@
+  const player = document.getElementById("player");
+  const icon = document.querySelector(".videobar-icon");
+
+  let youtubePlayer;
+  function onYouTubeIframeAPIReady() {
+    youtubePlayer = new YT.Player("player", {
+      height: "100%",
+      width: "100%",
+      videoId: "OhNyqv6mrCo",
+      playerVars: {
+        autoplay: 1,
+        controls: 1,
+      },
+      events: {
+        onReady: () => {
+          icon.style.display = "none";
+        },
+      },
+    });
+  }
+
+  player.addEventListener("click", () => {
+    if (!window.YT) {
+      const tag = document.createElement("script");
+      tag.src = "https://www.youtube.com/iframe_api";
+      const firstScriptTag = document.getElementsByTagName("script")[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    } else {
+      youtubePlayer.playVideo();
+    }
+  });
+
+  let buttons = document.querySelectorAll(".sibling-item");
+for (let btn of buttons) {
+    btn.onclick = function (e) {
+        e.preventDefault();
+        let active = document.querySelector(".active")
+        if (!(btn.classList.contains("active"))) {
+            btn.classList.add("active")
+            active.classList.remove("active")
+        }
+    }
+}
+
+
+
